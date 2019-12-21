d941f6a04155a56bbd0aebd8530b62a63abe3569
Jon Sessions <jon@sessionsdev.com>
2019-12-21 14:39:26 -0500

    admin-view scaffolding

---
+++ application/admin.py
@@ -0,0 +1,18 @@
+from flask import current_app as app
+from flask_admin import Admin
+from . import db
+from .models import User, Student, Mentor, SupportLog
+from flask_admin.contrib.sqla import ModelView
+
+
+admin = Admin(app, name='CodingNomads Mentor App', template_mode='bootstrap3')
+
+
+class UserView(ModelView):
+    # TODO: customize user views in admin panel
+    pass
+
+admin.add_view(ModelView(User, db.session, endpoint='user_admin'))
+admin.add_view(ModelView(Student, db.session, endpoint='student_admin'))
+admin.add_view(ModelView(Mentor, db.session, endpoint='mentor_admin'))
+admin.add_view(ModelView(SupportLog, db.session, endpoint='support_log_admin'))
