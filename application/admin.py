69d478c8ad733a4a5a53afa4a78591f5e85a4d0d
Jon Sessions <jon@sessionsdev.com>
2019-12-27 16:22:05 -0500

    Added analytics admin view boilerplate

--- application/admin.py
+++ application/admin.py
@@ -3,22 +3,29 @@ from flask_admin import Admin, BaseView, expose
 from . import db
 from .models import User, Student, Mentor, SupportLog
 from flask_admin.contrib.sqla import ModelView
+from .data_services import get_all_students
 
 
 admin = Admin(app, name='CodingNomads Mentor App', template_mode='bootstrap3')
 
 
 class UserView(ModelView):
-    # TODO: customize user views in admin panel
-    pass
+
+    can_delete = False
+    page_size = 50
+
 
 
 class AnalyticsView(BaseView):
     @expose('/')
     def index(self):
-        return 'Hello World'
+        users = [
+            {'user1': 'Joe'},
+            {'user2': 'Billy'}
+            ]
+        return self.render('admin/analytics.html', users=users)
 
-admin.add_view(ModelView(User, db.session, endpoint='user_admin'))
+admin.add_view(UserView(User, db.session, endpoint='user_admin'))
 admin.add_view(ModelView(Student, db.session, endpoint='student_admin'))
 admin.add_view(ModelView(Mentor, db.session, endpoint='mentor_admin'))
 admin.add_view(ModelView(SupportLog, db.session, endpoint='support_log_admin'))
