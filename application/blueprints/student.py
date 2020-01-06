c3d3ffc7917fc12d8813389cb59faae46f8a86bc
martin-martin <breuss.martin@gmail.com>
2020-01-06 14:46:24 +0100

    Add student overview

--- application/blueprints/student.py
+++ application/blueprints/student.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Blueprint, jsonify, request, render_template, flash, redirect, url_for, abort
 from application.utils import utc_to_local
 from application import db
@@ -16,7 +18,50 @@ def get_student(student_id):
 
     if data is None:
         return abort(404, description='Student not found')
-    return jsonify(data), 200
+
+    # TODO: change to proper backend calls
+    form = SupportForm()
+    content = json.loads("""{
+    "current_user": {
+        "first_name": "Gilad",
+        "last_name": "Gressel",
+        "is_admin": false,
+        "user_id": 1
+    },
+    "student": {
+        "aims": "wants to learn to frontend",
+        "id": 2,
+        "mentor_id": 3,
+        "mentor_name": "Gilad Gressel",
+        "preferred_learning": "discussions",
+        "start_date": "Fri, 13 Sep 2019 13:14:57 GMT",
+        "status": "student",
+        "user_id": 2,
+        "username": "johnny",
+        "email": "johnny@gmail.com",
+        "first_name": "Johnny",
+        "last_name": "Appleseed",
+        "learning_platform": "jseed",
+        "forum": "johnny",
+        "slack": "apple",
+        "time_zone": "Europe/London",
+        "courses": [
+            {
+                "id": 8,
+                "name": "Python Software Development",
+                "progress_percent": 80
+            }
+        ],
+        "preferred_days": {
+            "Mon": true, "Tue": true, "Wed": true,
+            "Thu": true, "Fri": true, "Sat": false, "Sun": false},
+        "preferred_start_time": "08:00:00",
+        "preferred_end_time": "17:00:00"
+    }
+}
+    """)
+    return render_template('student_profile.html', form=form, title=content['student']['username'], **content)
+    # return jsonify(data), 200
 
 
 # Log support for a given student
