5925f27e95cd6917460180c4ebbc5ff508afd936
martin-martin <breuss.martin@gmail.com>
2020-01-17 11:44:58 +0100

    Improve display, filtering, and search

--- application/admin.py
+++ application/admin.py
@@ -1,4 +1,6 @@
+from pytz import all_timezones_set
 from flask import current_app as app
+from flask import url_for
 from flask_admin import Admin, BaseView, expose
 from . import db
 from .models import User, Student, Mentor, SupportLog
@@ -6,14 +8,42 @@ from flask_admin.contrib.sqla import ModelView
 from .data_services import get_all_students
 
 
-admin = Admin(app, name='CodingNomads Mentor App', template_mode='bootstrap3')
+admin = Admin(app, name='CN Mentor Portal', template_mode='bootstrap3')
 
 
 class UserView(ModelView):
-
     can_delete = False
     page_size = 50
+    can_view_details = True
+    column_searchable_list = ['first_name', 'last_name', 'email']
+    column_filters = ['is_student', 'is_mentor']
+    create_modal = True
+    edit_modal = True
+    column_exclude_list = ['time_modified', 'time_created',
+                           'created_at', 'updated_at',
+                           'forum', 'password_hash', 'last_login']
+    form_excluded_columns = column_exclude_list.copy() + ['first_access', 'last_access']
+    form_choices = {
+        'timezone': [(tz, tz) for tz in all_timezones_set]
+    }
 
+
+class SupportLogView(ModelView):
+    column_searchable_list = ['mentor_id', 'student_id', 'support_type']
+    column_filters = ['mentor_id', 'student_id', 'comprehension', 'support_type', 'time_spent']
+    create_modal = True
+    edit_modal = True
+    column_exclude_list = ['updated_at']
+    #form_excluded_columns = column_exclude_list.copy() + ['created_at']
+    form_choices = {
+        'support_type': [  # TODO: centralize to make sure this stays up-to-date
+            ('call', 'Call'),
+            ('chat', 'Chat'),
+            ('meeting', 'Meeting'),
+            ('forum', 'Forum'),
+            ('email', 'Email')
+        ]
+    }
 
 
 class AnalyticsView(BaseView):
@@ -25,8 +55,9 @@ class AnalyticsView(BaseView):
             ]
         return self.render('admin/analytics.html', users=users)
 
-admin.add_view(UserView(User, db.session, endpoint='user_admin'))
-admin.add_view(ModelView(Student, db.session, endpoint='student_admin'))
-admin.add_view(ModelView(Mentor, db.session, endpoint='mentor_admin'))
-admin.add_view(ModelView(SupportLog, db.session, endpoint='support_log_admin'))
+
+admin.add_view(UserView(User, db.session, endpoint='users'))
+admin.add_view(ModelView(Student, db.session, endpoint='students'))
+admin.add_view(ModelView(Mentor, db.session, endpoint='mentors'))
+admin.add_view(SupportLogView(SupportLog, db.session, endpoint='logs'))
 admin.add_view(AnalyticsView(name='Analytics', endpoint='analytics'))
